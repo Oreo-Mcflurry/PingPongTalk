0a8187328f8e5e3e7c84a62b1d8e15d06abaf780
유인호 <yuiop1029@naver.com>
2024-06-04 09:54:43 +0900

    [UPDATE] Update KeychainManager

--- PingPongTalk/Service/Keychain/KeychainManager.swift
+++ PingPongTalk/Service/Keychain/KeychainManager.swift
@@ -9,22 +9,19 @@ import Foundation
 import KeychainSwift
 
 final class KeychainManager {
-	private let keychain: KeychainSwift
-
-	init() {
-		self.keychain = KeychainSwift()
-	}
+	static let keychain = KeychainSwift()
+	private init() { }
 
 	enum KeychainKeys: String {
 		case accessToken
 		case refreshToken
 	}
 
-	func save(_ value: String, with key: KeychainKeys) {
+	static func save(_ value: String, with key: KeychainKeys) {
 		keychain.set(value, forKey: key.rawValue)
 	}
 
-	func getData(with key: KeychainKeys) -> String? {
-		return keychain.get(key.rawValue)
+	static func getData(with key: KeychainKeys) -> String {
+		return keychain.get(key.rawValue) ?? ""
 	}
 }
